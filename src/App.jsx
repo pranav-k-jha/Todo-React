f762bac0f4f623d51e39a2999fbde19c0b9fa8ce
pranav-k-jha <pranav.jha@mail.concordia.ca>
2024-03-09 16:49:56 -0500

    added tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -41,13 +41,32 @@ const App = () => {
   }
 
   return (
-    <>
+    <div className="w-auto h-auto mt-8 p-10 rounded shadow">
       <TodoList todos={todos} toggleTodo={toggleTodo} />
-      <input ref={todoNameRef} type="text" />
-      <button onClick={handleAddTodo}>Add Todo</button>
-      <button onClick={handleClearTodos}>Clear Complete</button>
-      <div>{todos.filter((todo) => !todo.complete).length} left to do</div>
-    </>
+      <div className="flex items-center mt-4">
+        <input
+          className="px-2 py-1 border rounded mr-2 focus:outline-none"
+          ref={todoNameRef}
+          type="text"
+          placeholder="Add todo..."
+        />
+        <button
+          className="px-4 py-2 bg-green-500 text-white rounded-full w-200 focus:outline-none"
+          onClick={handleAddTodo}
+        >
+          Add Todo
+        </button>
+        <button
+          className="px-4 py-2 ml-2 bg-red-500 text-white focus:outline-none rounded-full w-200"
+          onClick={handleClearTodos}
+        >
+          Clear Complete
+        </button>
+      </div>
+      <div className="bg-blue-300 mt-4 text-gray-600">
+        {todos.filter((todo) => !todo.complete).length} left to do
+      </div>
+    </div>
   );
 };
 
