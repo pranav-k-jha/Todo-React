3d0f9a21eb0291bf7098a78675e20b7002a525a7
pranav-k-jha <pranav.jha@mail.concordia.ca>
2024-03-09 17:34:56 -0500

    added responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -41,23 +41,23 @@ const App = () => {
   }
 
   return (
-    <div className="w-[600px] mt-8 p-10 rounded shadow">
+    <div className="max-w-screen-lg mx-auto mt-8 p-6 sm:p-10 rounded shadow">
       <TodoList todos={todos} toggleTodo={toggleTodo} />
-      <div className="flex items-center mt-4">
+      <div className="flex flex-col sm:flex-row items-center mt-4">
         <input
-          className="px-2 py-1 border rounded mr-2 focus:outline-none"
+          className="flex-1 px-2 py-1 border rounded mb-2 sm:mr-2 sm:mb-0 focus:outline-none"
           ref={todoNameRef}
           type="text"
           placeholder="Add todo..."
         />
         <button
-          className="px-4 py-2 bg-green-500 text-white rounded-full w-200 focus:outline-none"
+          className="w-full sm:w-auto px-4 py-2 bg-green-500 text-white rounded-full focus:outline-none"
           onClick={handleAddTodo}
         >
           Add Todo
         </button>
         <button
-          className="px-4 py-2 ml-2 bg-red-500 text-white focus:outline-none rounded-full w-200"
+          className="w-full sm:w-auto mt-2 sm:mt-0 ml-0 sm:ml-2 px-4 py-2 bg-red-500 text-white focus:outline-none rounded-full"
           onClick={handleClearTodos}
         >
           Clear Complete
